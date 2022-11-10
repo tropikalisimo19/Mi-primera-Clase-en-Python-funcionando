f35a35057d2d66580c499a772e33f748b8ff7932
Brayan Erickson <116596296+tropikalisimo19@users.noreply.github.com>
2022-11-09 23:33:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,47 @@
 #Clase
 class Fraction():
-    ##Atributos##
+    ##ATRIBUTOS##
     numerator = 0
     denominator = 0
     ##Fin Atributos##
 
-    ##Constructor##
+    ##CONSTRUCTOR##
     def __init__(self,numerator,denominator):
         self.numerator = numerator
         self.denominator = denominator
-    ##Fin Constructor##
+    ##FIN CONSTRUCTOR##
 
     ##Metodos##
     def multiplication(self,other):
         numerator = self.numerator * other.numerator
         denominator = self.denominator * other.denominator
-        print("\nEl resultado de la multiplicacion:",numerator,"/",denominator)
+        print("\nLa multiplicación es:",numerator,"/",denominator)
 
     def division(self,other):
         numerator = self.numerator * other.denominator
         denominator = self.denominator * other.numerator
-        print("\nEl resultado de la division:",numerator,"/",denominator)
+        print("\nLa division es:",numerator,"/",denominator)
 
     def addition(self,other):
         if(self.denominator != other.denominator):
             denominator = self.denominator * other.denominator
             numerator = (self.numerator*other.denominator)+(other.numerator*self.denominator)
-            print("\nEl resultado de la suma:",numerator,"/",denominator)
+            print("\nLa suma es:",numerator,"/",denominator)
         else:
             numerator = self.numerator + other.numerator
-            print("\nEl resultado de la suma:",numerator,"/",self.denominator)
+            print("\nLa suma es:",numerator,"/",self.denominator)
 
     def subtraction(self,other):
         if(self.denominator != other.denominator):
             denominator = self.denominator * other.denominator
             numerator = (self.numerator*other.denominator)-(other.numerator*self.denominator)
-            print("\nEl resultado de la resta:",numerator,"/",denominator)
+            print("\nLa resta es:",numerator,"/",denominator)
         else:
             numerator = self.numerator - other.numerator
-            print("\nEl resultado de la resta:",numerator,"/",self.denominator)
+            print("\nLa resta es:",numerator,"/",self.denominator)
 
 
-#Iniciar variables
+    ##INICIALIZAR VARIABLES##
 numerator_1 = 0
 numerator_2 = 0
 denominator_1 = 0
@@ -49,39 +49,31 @@ denominator_2 = 0
 f_1 = 0
 f_2 = 0
 opcion = 0
-#Entrada de datos
+    ##ENTRADA DE DATOS##
 numerator_1 = int(input("Numerador fraccion A: "))
-while(denominator_1 == 0):
+int(input(denominator_1 == 0):
     denominator_1 = int(input("Denomiandor fraccion A: "))
-    if(denominator_1!=0):
+    int(input(denominator_1!=0):
         break
-    print("Ingrese un denominador distinto de 0")
+    print("Ingresar otro dato que no sea 0")
 numerator_2 = int(input("Numerador fraccion B: "))
-while(denominator_2 == 0):
+int(input(denominator_2 == 0):
     denominator_2 = int(input("Denominador fraccion B: "))
-    if(denominator_2!=0):
+    int(input(denominator_2!=0):
         break
-    print("Ingrese un denominador distinto de 0")
-#asignar variables
+    print("Ingresar otro dato que no sea 0")
+
 f_1 = Fraction(numerator_1, denominator_1)
 print("Fraccion A =",f_1.numerator,"/",f_1.denominator)
 f_2 = Fraction(numerator_2, denominator_2)
 print("Fraccion B =",f_2.numerator,"/",f_2.denominator)
-#Menu
-print("\n---------OPERACIONES MATEMATICAS CON FRACCIONES---------\n")
-print("SUMA(+)")
-print("RESTA(-)")
-print("MULTIPLICACION(*)")
-print("DIVISION(/)\n")
-print("---------------------------------------------------------\n")
-opcion = input("Ingrese la operacion a realizar (+ - * /):")
 
-match(opcion):
-    case '+':
-        f_1.addition(f_2)
-    case '-':
-        f_1.subtraction(f_2)
-    case '*':
-        f_1.multiplication(f_2)
-    case '/':
-        f_1.division(f_2)
+        ##MOSTRAR EN PANTALLA##
+print("\n========FRACCIONES MATEMATICAS========\n")
+print("Si es suma seleccione la opción: (+)")
+print("Si es resta seleccione la opción:(-)")
+print("Si es multiplicacion seleccione la opción:(*)")
+print("Si es division seleccione la opción:(/)\n")
+print("===============================================\n")
+opcion = input("Ingrese su opción (+ - * /):")
+
